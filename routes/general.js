8244845a620bbc8ea3566cd2c4e8f862d1f948c2
jul15xn <88789943+jul15xn@users.noreply.github.com>
2025-09-09 19:24:40 +0200

    Cleaned up page

--- routes/general.js
+++ routes/general.js
@@ -6,15 +6,12 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
-
 router.get('/contact', (req, res) => {
   const success = req.query.success === 'true';
   res.render('contact', { success });
 });
 
-
 router.post('/contact', (req, res) => {
-  console.log(req.body);
   const { name, email, message } = req.body;
   sendContactEmail(name, email, message);
   res.redirect('/contact?success=true');
