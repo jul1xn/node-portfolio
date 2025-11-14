5a84ceae216f5e13042d33b7146ae6980c64972e
jul15xn <88789943+jul15xn@users.noreply.github.com>
2025-11-14 14:46:59 +0100

    Preparation

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-const projectRoutes = require('./routes/projects');
-app.use('/projecten', projectRoutes);
-
-const adminRoutes = require('./routes/admin');
-app.use('/admin', adminRoutes);
-
-const generalRoutes = require('./routes/general');
-app.use('/', generalRoutes);
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
