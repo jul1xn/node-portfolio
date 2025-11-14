400a2612f6da294f63749a9ff0076207ad0e6b31
jul15xn <88789943+jul15xn@users.noreply.github.com>
2025-11-14 15:17:59 +0100

    Created general endpoints

--- routes/general.js
+++ routes/general.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
     res.render('general/index', { title: 'Home', links: constants.NAVBAR_LINKS, name: constants.WEBSITE_NAME });
 });
 
+router.get('/over-mij', (req, res) => {
+    res.render('general/overmij', { title: 'Over mij', links: constants.NAVBAR_LINKS, name: constants.WEBSITE_NAME });
+});
+
+router.get('/ervaring', (req, res) => {
+    res.render('general/ervaring', { title: 'Ervaring', links: constants.NAVBAR_LINKS, name: constants.WEBSITE_NAME });
+});
+
+router.get('/cv', (req, res) => {
+    res.render('general/cv', { title: 'CV', links: constants.NAVBAR_LINKS, name: constants.WEBSITE_NAME });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('general/contact', { title: 'Contact', links: constants.NAVBAR_LINKS, name: constants.WEBSITE_NAME });
+});
+
 module.exports = router;
