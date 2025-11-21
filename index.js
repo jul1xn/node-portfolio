ae0ecf13b7767bb82d957a3a45688df28f02edb3
jul15xn <88789943+jul15xn@users.noreply.github.com>
2025-11-21 20:20:38 +0100

    Verbeteringen met laadtijd

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(
+  express.static('public', {
+    maxAge: 86400000,
+    setHeaders: function (res, path) {
+      res.setHeader("Expires", new Date(Date.now() + 2592000000 * 30).toUTCString());
+    }
+  })
+
+);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 
 const generalRoutes = require('./routes/general');
 app.use('/', generalRoutes);
