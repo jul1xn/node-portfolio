be1119a6f3851e2028f2a335216f7796c3934521
Jul1xn <julian.verwoerd@pantronic.nl>
2026-05-20 16:58:16 +0200

    Footer test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Portfolio - Julian Verwoerd",
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="px-60 py-15 min-h-[calc(100vh-5rem)]">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
