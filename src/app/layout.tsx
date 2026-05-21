c9ee9065ce33e232c27a85fa9f1400cc4d1c3642
Jul1xn <julian.verwoerd@pantronic.nl>
2026-05-21 09:33:10 +0200

    Added .env support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
+import 'dotenv/config';
 
 export const metadata: Metadata = {
   title: "Portfolio - Julian Verwoerd",
