e3616f9b4aaa84b84795f839ccda7fde8fa90ab3
Jul1xn <julian.verwoerd@pantronic.nl>
2026-05-21 12:30:26 +0200

    Created scrollable brands list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,77 @@
 import Counter from "@/components/Counter";
 import ProjectCard from "@/components/ProjectCard";
 import Seperator from "@/components/Seperator";
+import * as Brands from "@/utils/brands.utils";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+
+const brands = [
+  {
+    name: "Next.js",
+    href: "https://nextjs.org/",
+    Icon: Brands.Nextjs,
+  },
+  {
+    name: "Node.js",
+    href: "https://nodejs.org/",
+    Icon: Brands.Nodejs,
+  },
+  {
+    name: "TypeScript",
+    href: "https://www.typescriptlang.org/",
+    Icon: Brands.TypeScript,
+  },
+  {
+    name: "JavaScript",
+    href: "https://developer.mozilla.org/docs/Web/JavaScript",
+    Icon: Brands.JavaScript,
+  },
+  {
+    name: "Java",
+    href: "https://www.java.com/",
+    Icon: Brands.Java,
+  },
+  {
+    name: "C#",
+    href: "https://dotnet.microsoft.com/languages/csharp",
+    Icon: Brands.CS,
+  },
+  {
+    name: "C++",
+    href: "https://en.wikipedia.org/wiki/C%2B%2B",
+    Icon: Brands.CPP,
+  },
+  {
+    name: "Unity",
+    href: "https://unity.com/",
+    Icon: Brands.Unity,
+  },
+  {
+    name: "Lua",
+    href: "https://lua.org/",
+    Icon: Brands.Lua,
+  },
+  {
+    name: "PHP",
+    href: "https://www.php.net/",
+    Icon: Brands.Php,
+  },
+  {
+    name: "MySQL",
+    href: "https://www.mysql.com/",
+    Icon: Brands.MySQL,
+  },
+  {
+    name: "PostgreSQL",
+    href: "https://www.postgresql.org/",
+    Icon: Brands.PostgreSQL,
+  },
+  {
+    name: "Python",
+    href: "https://www.python.org/",
+    Icon: Brands.Python,
+  },
+]
 
 export default function Home() {
   return (
@@ -49,9 +118,33 @@ export default function Home() {
       <div className="mt-10">
         <h1 className="text-center font-light text-4xl mb-8">Uitgelichte projecten</h1>
         <div className="grid grid-cols-3 grid-rows-1 gap-5">
-          <ProjectCard id="portfolio-website"/>
-          <ProjectCard id="school-examen-game"/>
-          <ProjectCard id="minecraft-clone"/>
+          <ProjectCard id="portfolio-website" />
+          <ProjectCard id="school-examen-game" />
+          <ProjectCard id="minecraft-clone" />
+        </div>
+        <div className="my-5 mx-auto text-center align-center">
+          <Link href="/over-mij" className="inline-flex items-center gap-2 bg-purple-700 text-white px-5 py-3 text-xl rounded-full cursor-pointer transition-colors hover:bg-purple-900">
+            <ArrowRight />
+            Bekijk alle projecten
+          </Link>
+        </div>
+      </div>
+      <Seperator />
+      <div className="mb-3 mt-5">
+        <h1 className="text-center font-light text-4xl mb-8">Tools en talen die ik gebruik</h1>
+        <div className="relative w-full overflow-hidden">
+          <div className="group flex w-max gap-10 animate-scroll-brands">
+            {[...brands, ...brands].map((brand, index) => (
+              <Link
+                key={`${brand.name}-${index}`}
+                href={brand.href}
+                target="_blank"
+                className="shrink-0 transition-opacity group-hover:opacity-50 hover:opacity-100!"
+              >
+                <brand.Icon className="h-30 w-30" />
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </>
