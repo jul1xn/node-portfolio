94cad30a606282578d8f4317d2b003d46f4d6ed0
Jul1xn <julian.verwoerd@pantronic.nl>
2026-07-16 16:39:25 +0200

    Fixed inconsistent styling

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -9,7 +9,7 @@ export default function ImageCard({ src, description }: Props) {
     const hasDescription = Boolean(description);
 
     return (
-        <div className="overflow-hidden rounded-3xl border border-neutral-800 bg-[#0b0a14] shadow-xl my-5">
+        <div className="overflow-hidden rounded-3xl border border-neutral-800 bg-[#111111] shadow-xl my-5">
             <div className="w-full h-96 overflow-hidden bg-neutral-900">
                 <a
                     href={src}
